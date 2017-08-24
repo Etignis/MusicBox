11ac75a15a80aef61bf131a94758edfc0c064c4e
Etignis <etignis@gmail.com>
2017-08-24 10:33:11 +0300

    wip

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   
+  Vue.component('playlist-source-option', {
+    props: {
+      title: {
+        type: String,
+        default: "источник"
+      }
+    },
+    template: "<div><label><input type='checkbox'> {{title}}</label></div>"
+  });
+  
   Vue.component('playlist-tag', {
     props: {
       title: {
@@ -27,6 +37,9 @@ $(document).ready(function(){
     	random: function(oEvent) {
       	alert('random');
       },
+    	fix: function(oEvent) {
+      	alert('fix');
+      },
       toggleSettings: function(oEvent) {
         	this.show_settings = !this.show_settings;
       },
@@ -35,30 +48,118 @@ $(document).ready(function(){
       }
     },
     template: "<div class='PlayListTag' v-bind:class='{ active: isActive }' v-on:click='toggleActive'>\
-          <div class='title'>{{title}}</div>\
-          <div v-if='show_buttons === true' class='buttons'>\
-            <button class='btn' title='Перемешать плейлист' v-on:click.stop='random'>\
-              <i class='fa fa-random' aria-hidden='true'></i>\
-            </button>\
-            <button class='btn' title='Закрепить текущую мелодию'>\
-              <i class='fa fa-thumb-tack' aria-hidden='true'></i>\
-            </button>\
-            <button class='btn' title='Выбрать источники' v-on:click.stop='toggleSettings'>\
-              <i class='fa fa-cog' aria-hidden='true'></i>\
-            </button>\
+          <div class='inner'>\
+            <div class='title'>{{title}}</div>\
+            <div v-if='show_buttons === true' class='buttons'>\
+              <button class='btn' title='Перемешать плейлист' v-on:click.stop='random'>\
+                <i class='fa fa-random' aria-hidden='true'></i>\
+              </button>\
+              <button class='btn' title='Закрепить текущую мелодию' v-on:click.stop='fix'>\
+                <i class='fa fa-thumb-tack' aria-hidden='true'></i>\
+              </button>\
+              <button class='btn' v-bind:class='{ active: show_settings }' title='Выбрать источники' v-on:click.stop='toggleSettings'>\
+                <i class='fa fa-cog' aria-hidden='true'></i>\
+              </button>\
+            </div>\
           </div>\
           <div class='settings' v-if='show_settings === true'>\
-          	Настройки\
+          	<div class='s-title'>Источники</div>\
+            <slot></slot>\
           </div>\
         </div>"
   });
 
-  
-  var PlayListGroupEmotions = new Vue({
-    el: '#PlayListGroupEmotions'
+  Vue.component('playlist-group', {
+    props: {
+      title: {
+        type: String,
+        default: "заголовок"
+      },
+      styleclass: {
+        type: String,
+        default: "заголовок"
+      },
+      selectedTagId: {
+        type: String
+      }
+    },
+    methods: {
+      selectTag: function(oEvent) {
+        console.log("click tag")
+      }
+    },
+    template: "<div class='PlayListGroup' v-bind:class='[styleclass]'>\
+      <div class='PlayListGroupTitle'>\
+        <div class='title'>\
+           {{title}}\
+        </div>\
+      </div>\
+      <div class='flexContent' id='PlayListGroupEmotions'>\
+        <slot></slot>\
+      </div>\
+    </div>"
+  });
+
+  Vue.component('playlists', {
+    template: '<div class="PlayLists">\
+       <slot></slot>\
+    </div>'
   });
   
-  var PlayListGroupPlaces = new Vue({
-    el: '#PlayListGroupPlaces'
+  
+  var player = new Vue({
+    el: '#app',
+    data: {
+      arr: [
+        {
+          id: "1",
+          title: "Настоение",
+          styleclass: "Emotions",
+          selectedTegId: "11",
+          tags: [
+            {
+              id: "11",
+              title: "Фон",
+              src: [
+                {
+                  title: "src1",
+                  id: "111"
+                },
+                {
+                  title: "src2",
+                  id: "112"
+                },
+                {
+                  title: "src3",
+                  id: "113"
+                }
+              ]
+            },
+            {
+              id: "12",
+              title: "Экшн",
+              src: [
+                {
+                  title: "src3",
+                  id: "121"
+                },
+                {
+                  title: "src4",
+                  id: "122"
+                }
+              ]
+            }
+          ]
+        }, 
+        {
+          id: "2",
+          title: "Места",
+          styleclass: "Places",
+          tags: [
+            
+          ]
+        }
+      ]
+    }
   });
 }); 
